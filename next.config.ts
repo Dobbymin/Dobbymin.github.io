3077c120125212001d060f28207139a41b5879af
Dobbymin <gmin990624@gmail.com>
2025-05-08 01:29:22 +0900

    chore: github page 배포 기능 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://Dobbymin.github.io"
+      : "",
 };
 
 export default nextConfig;
