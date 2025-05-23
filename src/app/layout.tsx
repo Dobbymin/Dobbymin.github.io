c632bbeed3e0ba9a39e8789c2a13ace26e80e097
Dobbymin <gmin990624@gmail.com>
2025-05-23 19:18:46 +0900

    feat: 레이아웃 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Lemon } from 'next/font/google';
 import localFont from 'next/font/local';
 
 import { Toaster } from '@/shared';
+import { BlogLayout } from '@/widgets';
 
 import './globals.css';
 
@@ -38,7 +39,7 @@ const lemon = Lemon({
 });
 
 export const metadata: Metadata = {
-  title: 'Dobby is Free~',
+  title: 'Dobby is Free',
   description: '김도비의 개인 블로그',
 };
 
@@ -52,7 +53,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${lemon.variable} ${pretendard.variable} ${suit.variable} antialiased`}
       >
-        <main>{children}</main>
+        <BlogLayout>{children}</BlogLayout>
+        {/* <main className='w-full items-center justify-center'></main> */}
         <Toaster />
       </body>
     </html>
