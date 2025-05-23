649f5bc052e23d00d853e6e2eb3d68820d41396a
Dobbymin <gmin990624@gmail.com>
2025-05-23 19:17:54 +0900

    feat: 헤더 ui 구현

--- src/shared/components/features/Header.tsx
+++ src/shared/components/features/Header.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+
+import { routerPath } from '@/shared/constants';
+import { Sun } from 'lucide-react';
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='fixed top-0 right-0 left-0 z-100 flex h-header items-center justify-between border-b-2 border-[#AFAFAF] bg-white px-header shadow-sm transition-colors'>
+      <Link href={routerPath.main}>
+        <p className='font-lemon text-3xl'>Dobblog</p>
+      </Link>
+      <div className='flex items-center justify-between gap-5'>
+        <Link href={routerPath.about}>
+          <p className='text-xl font-semibold'>About</p>
+        </Link>
+        <Sun className='cursor-pointer' />
+      </div>
+    </header>
+  );
 };
--- src/widgets/layout/BlogLayout.tsx
+++ src/widgets/layout/BlogLayout.tsx
@@ -1,3 +1,15 @@
-export const BlogLayout = () => {
-  return <div>BlogLayout</div>;
+import { Footer, Header } from '@/shared';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const BlogLayout = ({ children }: Props) => {
+  return (
+    <div className='flex min-h-screen w-full flex-col items-center justify-center'>
+      <Header />
+      <div className='flex h-content w-full'>{children}</div>
+      <Footer />
+    </div>
+  );
 };
