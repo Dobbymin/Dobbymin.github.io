a5e284e6c1126e249e517153ee5fb00fd33908b0
Dobbymin <gmin990624@gmail.com>
2025-10-25 23:29:03 +0900

    refactor(page.tsx): PostCard 및 Tabs 제거, PostList로 대체

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
 import { getAllPosts } from '@/shared/lib';
 
-import { PostCard, Tabs } from '@/shared';
-import { Grid } from '@/widgets';
+import { PostList } from '@/widgets';
 
 export default function Home() {
   const allPosts = getAllPosts();
 
-  return (
-    <div className='flex flex-col items-center justify-center gap-10'>
-      <Tabs />
-      <Grid
-        className='w-full px-4 md:px-8 lg:px-0'
-        cols='grid-cols-1 md:grid-cols-2 lg:grid-cols-[270px_270px_270px]'
-        gap={5}
-        justifyContent='center'
-        maxWidth='7xl'
-      >
-        {allPosts.map((post) => (
-          <PostCard key={post.slug} post={post} />
-        ))}
-      </Grid>
-    </div>
-  );
+  return <PostList posts={allPosts} />;
 }
