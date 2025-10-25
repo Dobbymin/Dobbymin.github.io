055dda91f65520e7430e15fa55f308c76e90c727
Dobbymin <gmin990624@gmail.com>
2025-10-25 23:43:26 +0900

    fix(Footer): Footer 컴포넌트 스타일 개선 및 반응형 텍스트 추가

--- src/shared/components/features/layout/Footer.tsx
+++ src/shared/components/features/layout/Footer.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link';
 
 export const Footer = () => {
   return (
-    <footer className='flex h-footer w-full items-center justify-center bg-blog-gray-300'>
+    <footer className='flex h-footer w-full items-center justify-center bg-blog-gray-300 px-4'>
       <div className='flex flex-col items-start justify-between gap-1'>
-        <p className='text-blog-gray-600'>
+        <p className='text-sm text-blog-gray-600 md:text-base'>
           Copyright &copy; 2025 <b>Dobbymin</b> All rights reserved.
         </p>
-        <div className='flex items-center justify-center gap-1 font-fira-code text-blog-gray-600'>
+        <div className='flex flex-wrap items-center justify-center gap-1 font-fira-code text-sm text-blog-gray-600 md:text-base'>
           <span>gmin0701@knu.ac.kr</span>
           <span className='font-bold'>.</span>
-          <span>+82-10-3095-7259</span>
-          <span className='font-bold'>.</span>
+          <span className='hidden md:inline'>+82-10-3095-7259</span>
+          <span className='hidden font-bold md:inline'>.</span>
           <Link
             className='hover:font-bold hover:underline'
             href='https://github.com/Dobbymin'
