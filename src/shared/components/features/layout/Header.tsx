f6a894c71f61eba38009d82e2ddb27767cc4aa7b
Dobbymin <gmin990624@gmail.com>
2025-10-25 23:30:02 +0900

    refactor(Header): Header 컴포넌트 구조 개선 및 스타일 수정

--- src/shared/components/features/layout/Header.tsx
+++ src/shared/components/features/layout/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { Search, Sun } from 'lucide-react';
@@ -7,22 +8,35 @@ import { Input } from '../../ui';
 
 export const Header = () => {
   return (
-    <header className='fixed top-0 right-0 left-0 z-100 flex h-header items-center justify-between bg-blog-gray-500 px-header transition-colors'>
-      <Link href={routerPath.main}>
-        <p className='font-lemon text-3xl'>Dobblog</p>
-      </Link>
-      <div className='absolute top-1/2 left-1/2 hidden w-[400px] max-w-[60vw] -translate-x-1/2 -translate-y-1/2 items-center justify-center gap-3 rounded-sm bg-blog-gray-100 md:flex'>
-        <Input
-          className='border-none bg-transparent text-sm text-blog-gray-500 shadow-none outline-none placeholder:text-blog-gray-400 focus:border-none focus:ring-0 focus:outline-none'
-          type='text'
-        />
-        <Search className='mr-3 text-blog-gray-500' size={20} />
-      </div>
-      <div className='flex items-center justify-between gap-5'>
-        <Link href={routerPath.about}>
-          <p className='font-lemon text-xl'>About</p>
+    <header className='fixed top-0 right-0 left-0 z-100 flex items-center justify-center bg-blog-gray-500 transition-colors'>
+      <div className='relative flex h-header w-full max-w-7xl items-center justify-between px-4 md:px-8'>
+        <Link href={routerPath.main}>
+          <div className='flex items-center gap-4'>
+            <Image
+              alt='Dobblog Logo'
+              className='rounded-full'
+              height={40}
+              src='/logo.png'
+              width={40}
+            />
+            <p className='hidden font-lemon text-xl md:block md:text-3xl'>
+              Dobblog
+            </p>
+          </div>
         </Link>
-        <Sun className='cursor-pointer text-blog-pink' />
+        <div className='absolute top-1/2 left-1/2 hidden w-[400px] max-w-[60vw] -translate-x-1/2 -translate-y-1/2 items-center justify-center gap-3 rounded-sm bg-blog-gray-100 md:flex'>
+          <Input
+            className='border-none bg-transparent text-sm text-blog-gray-500 shadow-none outline-none placeholder:text-blog-gray-400 focus:border-none focus:ring-0 focus:outline-none'
+            type='text'
+          />
+          <Search className='mr-3 text-blog-gray-500' size={20} />
+        </div>
+        <div className='flex items-center justify-between gap-3 md:gap-5'>
+          <Link href={routerPath.about}>
+            <p className='font-lemon text-base md:text-xl'>About</p>
+          </Link>
+          <Sun className='cursor-pointer text-blog-pink' size={20} />
+        </div>
       </div>
     </header>
   );
