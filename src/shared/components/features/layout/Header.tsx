fa90241e76c435f741ee62b8dc9145ddf12f384d
Dobbymin <gmin990624@gmail.com>
2025-10-25 18:48:17 +0900

    feat(header): 헤더 컴포넌트 추가

--- src/shared/components/features/layout/Header.tsx
+++ src/shared/components/features/layout/Header.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 
-import { routerPath } from '@/shared/constants';
 import { Search, Sun } from 'lucide-react';
 
-import { Input } from '../ui';
+import { routerPath } from '../../../constants';
+import { Input } from '../../ui';
 
 export const Header = () => {
   return (
@@ -11,7 +11,7 @@ export const Header = () => {
       <Link href={routerPath.main}>
         <p className='font-lemon text-3xl'>Dobblog</p>
       </Link>
-      <div className='absolute top-1/2 left-1/2 hidden w-[400px] max-w-[60vw] -translate-x-1/2 -translate-y-1/2 items-center justify-center gap-3 rounded-sm bg-blog-gray-300 md:flex'>
+      <div className='absolute top-1/2 left-1/2 hidden w-[400px] max-w-[60vw] -translate-x-1/2 -translate-y-1/2 items-center justify-center gap-3 rounded-sm bg-blog-gray-100 md:flex'>
         <Input
           className='border-none bg-transparent text-sm text-blog-gray-500 shadow-none outline-none placeholder:text-blog-gray-400 focus:border-none focus:ring-0 focus:outline-none'
           type='text'
