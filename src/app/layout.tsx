69a9465ef6c9f7c9ea407c2823174c23b0baaaae
Dobbymin <gmin990624@gmail.com>
2025-10-26 01:33:41 +0900

    feat: meta data 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,8 +39,64 @@ const lemon = Lemon({
 });
 
 export const metadata: Metadata = {
-  title: 'Dobby is Free',
-  description: '김도비의 개인 블로그',
+  title: {
+    default: 'Dobby is Free',
+    template: '%s | Dobby is Free',
+  },
+  description:
+    '김도비의 개발 블로그 - 프론트엔드 개발, 웹 기술, 그리고 일상의 기록',
+  keywords: [
+    '개발 블로그',
+    '프론트엔드',
+    'Next.js',
+    'React',
+    'TypeScript',
+    '김도비',
+    'Dobby',
+  ],
+  authors: [{ name: '김도비', url: 'https://github.com/Dobbymin' }],
+  creator: '김도비',
+  publisher: '김도비',
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    url: 'https://dobbymin.github.io',
+    title: 'Dobby is Free',
+    description:
+      '김도비의 개발 블로그 - 프론트엔드 개발, 웹 기술, 그리고 일상의 기록',
+    siteName: 'Dobby is Free',
+    images: [
+      {
+        url: '/logo.png',
+        width: 1200,
+        height: 630,
+        alt: 'Dobby is Free Logo',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Dobby is Free',
+    description:
+      '김도비의 개발 블로그 - 프론트엔드 개발, 웹 기술, 그리고 일상의 기록',
+    images: ['/logo.png'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: '/logo.png',
+    shortcut: '/logo.png',
+    apple: '/logo.png',
+  },
 };
 
 export default function RootLayout({
